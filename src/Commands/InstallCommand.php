f146846fd8e2cc9d911bd979682864d5f93e15c6
xgbnl <745502274@qq.com>
2022-12-14 10:20:28 +0800

    chore: 发布指令变更

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -11,7 +11,7 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'business:publish';
+    protected $signature = 'fleet:publish';
 
     /**
      * The console command description.
